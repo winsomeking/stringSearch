756b427aa0da09ce3c27aa9879b27461741a459f
Berty <albertmelb@hotmail.com>
2013-10-23 13:25:00 +1100

    Final version,without trace back on edit distance

---
+++ ReadME.txt
@@ -0,0 +1,10 @@
+Read Me:
+
+To run the program: 
+
+python ./searchMaster.py
+
+please make sure all the test files are in the same directory.
+The program will search each query by trie, out put the result to a file called trieOutput.txt .
+Then it will start searching by edit distance, out put the result to another file editDistanceOutput.txt.
+Trie dictionary is also printed out to a file called trie.txt.
--- searchMaster.py
+++ searchMaster.py
@@ -6,14 +6,14 @@
 Reference: 1, http://stackoverflow.com/questions/11015320/how-to-create-a-trie-in-python/11015381#11015381
            2, http://en.wikipedia.org/wiki/Levenshtein_distance
            3, http://www.stanford.edu/class/cs124/lec/med.pdf
-           4, http://en.wikipedia.org/wiki/Trie
-           5, http://en.wikipedia.org/wiki/Smith%E2%80%93Waterman_algorithm
-//FIX ME:
+           4, Trie: http://en.wikipedia.org/wiki/Trie
+           5, Smith Waterman alogrithm: http://en.wikipedia.org/wiki/Smith%E2%80%93Waterman_algorithm
+
 """
 
-import nltk,re, pprint,time 
+import nltk,re, pprint,time,sys 
 
-
+#Helper method
 def equal(char1, char2):
     """ Smith-Waterman algorithm/local edit distance
        match = 2
@@ -24,25 +24,21 @@ def equal(char1, char2):
     else:
         return -1
 
+
 def localEditDistance(query,word):
-    # print len(query),len(word)
+    """ Return a tuple, (local distance, word) """  
     matrix = [ [0 for elem in xrange(len(word)+1)] for elem in xrange(len(query)+1) ] 
     maxI,maxJ = 0,0
     highestScore = 0
     for i in xrange(1,len(query)+1):
         for j in xrange(1,len(word)+1):
-            # print i,j
             matrix[i][j] = max(matrix[i-1][j]-1, matrix[i][j-1]-1,matrix[i-1][j-1] + equal(query[i-1],word[j-1]),0) 
-            # print matrix[i][j]
             if highestScore <= matrix[i][j]:
                 highestScore = matrix[i][j]
                 maxI = i
                 maxJ = j
     #  Start trace back from matrix[maxI][maxJ]
     #  Trace back
-    
-
-
     return highestScore, word #localAlignString
  
 
@@ -60,7 +56,7 @@ def buildTrieFromWords(*words):
     return trie
 
 def findMostMatchString (word, trie):
-    """Allowing mismatch 2"""   
+    """Return the most matched string."""   
     mismatchString = ""
     mismatchScore  = 9999
   
@@ -70,49 +66,62 @@ def findMostMatchString (word, trie):
              mismatchScore = tempScore
              mismatchString = k
     if( len(trie[mismatchString]) == 0): 
-        #print mismatchString + ':score: ' + str(mismatchScore) 
         return mismatchString,mismatchScore
     else:
         return findMostMatchString(word,trie[mismatchString])
-
-
-    # return mismatchString,mismatchScore 
-                
+                 
 	
 def searchQueryFromTrie(word,trie):
     """Recursively search an query from the trie, 
        return a tuple (result, score), 
        Score is the number of mismatch"""
     #  For loop is trying to find a match, if reach the end of file,the most matched one is returned.
-    mostMatchKey = ""
-    mismatches = 9999
+    currentMismatches = 9999
     for k in trie.iterkeys():
-        # print k
         if word == k:
             return k,0
         elif word.startswith(k):
             if len(trie[k])!= 0:
                 return searchQueryFromTrie(word,trie[k])
-            else:#End of trie
-                return k, findMismatchScore(word,k)
+            else: #End of trie
+                currentMismatches = findMismatchScore(word,k)
+        else: 
+            tempScore = findMismatchScore(word,k)
+            if  tempScore <= 2:
+                return k, tempScore    
+                # else:
+                #     return "NO", 2
+    return k, currentMismatches
+  
+
+def findMostMatchString(word,trie):
+    mostMatchKey = "NO"
+    mismatches = 2
+
     for k in trie.iterkeys():
         if len(trie[k]) == 0 :
             tempScore = findMismatchScore(word,k)
-            if tempScore < mismatches:
-                mismatches = tempScore
-                mostMatchKey = k
-        else: 
-            result = findMostMatchString(word,trie[k])
-            tempMatchString = result[0]
-            tempScore = result[1]
-            if tempScore <= mismatches :
-                mismatches = tempScore
-                mostMatchKey = tempMatchString
+            if tempScore <= mismatches:
+               mostMatchKey = k
+               mismatches = tempScore     
+            return mostMatchKey,mismatches
+        else:
+            tempScore = findMismatchScore(word,k)
+            if tempScore <= mismatches:
+               mismatches = tempScore
+               mostMatchKey = k
+               return findMostMatchString(word,trie[k])
+            else:   
+                newSearch = findMostMatchString(word,trie[k])
+                if newSearch[1] <= 2:
+                    mostMatchKey = newSearch[0]
+                    mismatches   = newSearch[1]
+
     return mostMatchKey,mismatches
 
 
 def findMismatchScore(word,candidate):
-    """return the mismatch of two strings"""
+    """return the mismatch score of two strings"""
     if len(word) > len(candidate):
         while len(candidate) != len(word):
             candidate = candidate + " "
@@ -130,60 +139,59 @@ def findMismatchScore(word,candidate):
 def searchByTrie(trie):
     """Read from query file and search each query from trie, output query, result and time"""
     f = open('surnames.txt')
-    output = open('outputNames.txt','a')
+    output = open('trieOutput.txt','a')
     for line in f:
-        # print line
         word = line.strip()
-        # print word
         startTime = time.time()
         result = searchQueryFromTrie(word,trie)
+        if result[1] > 2:
+            result = findMostMatchString(word,trie)
         endTime   = time.time()
         usedTime  = endTime - startTime
-        #print word + ': ' + result[0] + ' ' + str(result[1]) + ' ' + str(usedTime)
-        output.write(word + ': ' + result[0]  + ' ' + str(result[1]) + ' ' + str(usedTime) + '\n')
+        print word + ': result: ' + result[0]  + ' score: ' + str(result[1]) + ' time: ' + str(usedTime) 
+        output.write(word + ': result: ' + result[0]  + ' score: ' + str(result[1]) + ' time: ' + str(usedTime) + '\n')
 
 def searchByEditDistance(*words):
 
-    f = open('sur.txt')
+    f = open('surnames.txt')
     output = open('editDistanceOutput.txt','a')
     for line in f:
         query = line.strip()
         localDistance = 0
         bestMatchKey = "placeholder"
+        startTime = time.time()
         for elem in words:
             result = localEditDistance(query,elem)
             if result[0] >= localDistance:
                 localDistance = result[0]
                 bestMatchKey = result[1]        
-        print query + ' : ' + bestMatchKey + ' ' + str(localDistance)
-        output.write(query + ' : ' + bestMatchKey + ' ' + str(localDistance) + '\n') 
+        endTime = time.time()
+        usedTime = endTime - startTime
+        print query + ' : result : ' + bestMatchKey + ' : score : ' + str(localDistance) + ' time: ' + str(usedTime)
+        output.write(query + ' : result : ' + bestMatchKey + ' : score : ' + str(localDistance) + ' time: ' + str(usedTime) + '\n') 
 
-
-
-#    print bestMatchKey,localDistance
 
 if __name__ == "__main__":
     f = open('turgenev.txt')
-    #print f.read()
     raw = f.read()
-    #tokens = nltk.word_tokenize(raw)
-    #for w in tokens:    
-	#    w.lower()
     raw.lower()
     p = re.compile('[^a-z ]')
-#    p.search(raw)
     r = re.sub(p,'',raw)    
-    #print r
+    print "Preprocessing file..."
     tokens = nltk.word_tokenize(r)
-    #output_file = open('processedFile.txt','w')
-    #for w in tokens:
-    #    output_file.write(w + '\n')
-    # print buildTrieFromWords(*tokens)
+    print "Tokenized."
+    print "Start building Trie"
     trie = buildTrieFromWords(*tokens)
-    #print trie
-    #searchByTrie(trie)
-    #print "Searching from trie"
+    print "Trie size: " + str(sys.getsizeof(trie)) + " bytes"
+    print "Finished building Trie"
+    output = open('trie.txt','w')
+    output.write(str(trie))
+    print "Start searching by Trie."
+    searchByTrie(trie)
+    print "Searching by Trie is done."
+    print "Searching by edit distance."
     searchByEditDistance(*tokens)
-    #print "Searching by edit distance"
+    print "Searching by edit distance is done."
+
        
      
